8a04351c6f69e29c7b343c9edd1d7f2ed43eec1e
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-06-14 20:41:09 -0500

    Complete

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type a random word and pres Enter to start developer tea time");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("Its' developer tea time!");
+    }
     public static void main(String[] args) {
-
+        announceDeveloperTeaTime();
     }
 
 }
